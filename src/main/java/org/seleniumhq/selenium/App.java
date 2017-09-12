223ec03d120b965eba5c04a80f9637eb5ed866c3
Chris gutierrez <chris201234@gmail.com>
2017-09-12 15:24:04 -0500

    modified:   src/main/java/org/seleniumhq/selenium/App.java found another website to test on utilizing jsoup to scrape metadata

--- src/main/java/org/seleniumhq/selenium/App.java
+++ src/main/java/org/seleniumhq/selenium/App.java
@@ -7,9 +7,17 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
-public class App {
-    public static void main(String[] args) {
+import java.io.Exception;
 
+import org.jsoup.jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+public class App {
+    public static void main(String[] args) throws InterruptedException{
+/*
         // Create a new instance of the Firefox driver
         // Notice that the remainder of the code relies on the interface,
         // not the implementation.
@@ -17,13 +25,24 @@ public class App {
         System.setProperty("webdriver.gecko.driver", "/usr/bin/geckodriver");
         driver = new FirefoxDriver();
         // And now use this to visit Google
-        driver.navigate().to("http://thiagomarzagao.com/2013/11/12/webscraping-with-selenium-part-1/");
-
-        
-
-
+//        driver.navigate().to("http://thiagomarzagao.com/2013/11/12/webscraping-with-selenium-part-1/");
+//        driver.navigate().to("https://finance.yahoo.com/quote/MCD?p=MCD");
+        driver.navigate().to("http://www.marketwatch.com/investing/stock/AAPL");\
         //Close the browser
         driver.quit();
-
     }
+
+    */
+//Jsoup webscrape Strategy
+        try{
+            Document doc = jsoup.connect("http://www.marketwatch.com/investing/stock/AAPL").get();
+            String title = doc.title();
+            System.out.println("title: "+ title);
+            Element links = doc
+
+
+
+
+        }
+
 }
