4b6edade8e6c36e475ef66dc3ced2a0afd6be274
Chris gutierrez <chris201234@gmail.com>
2017-10-03 18:00:52 -0500

    Made Changes to use a yahooAPI that works
    setting up a simple test case
       -todo create CSV FILES

--- src/main/java/org/seleniumhq/selenium/App.java
+++ src/main/java/org/seleniumhq/selenium/App.java
@@ -1,5 +1,6 @@
 package org.seleniumhq.selenium;
 import java.io.IOException;
+import java.util.Calendar;
 import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -13,36 +14,21 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
 
 
 public class App {
     public static void main(String[] args) throws IOException {
-/*
-        // Create a new instance of the Firefox driver
-        // Notice that the remainder of the code relies on the interface,
-        // not the implementation.
-        WebDriver driver;
-        System.setProperty("webdriver.gecko.driver", "/usr/bin/geckodriver");
-        driver = new FirefoxDriver();
-        // And now use this to visit Google
-//        driver.navigate().to("http://thiagomarzagao.com/2013/11/12/webscraping-with-selenium-part-1/");
-//        driver.navigate().to("https://finance.yahoo.com/quote/MCD?p=MCD");
-        driver.navigate().to("http://www.marketwatch.com/investing/stock/AAPL");\
-        //Close the browser
-        driver.quit();
-    }
 
-    */
-//Jsoup webscrape Strategy
-        String url = "http://www.marketwatch.com/investing/stock/AAPL";
-        Document doc = Jsoup.connect(url).get();
-        String TickerSymbol = doc.select("meta[name=tickerSymbol]").first().attr("content");
+        Calendar from = Calendar.getInstance();
+        Calendar to= Calendar.getInstance();
+        from .add(Calendar.YEAR,-12);
+        Stock tesla = YahooFinance.get("AAPL", true);
+         String text;
+        System.out.println(tesla.getHistory(from,to));
 
-        String quote = doc.select("meta[name=price]").first().attr("content");
 
-        String priceChange = doc.select("meta[name=priceChange]").first().attr("content");
-        System.out.println("STOCK SYMBOL: " + TickerSymbol );
-        System.out.println("STOCK PRICE: "+quote +" CHANGE: "+ priceChange);
 
 
 
