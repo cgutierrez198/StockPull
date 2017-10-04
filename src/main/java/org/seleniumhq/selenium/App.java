e619acc1613a1ec96e9aadc066cece54710d265d
cgutierrez198 <chris_201234@yahoo.com>
2017-10-03 19:38:57 -0500

    Added csv files Had main parse the file testing how much we can pull

--- src/main/java/org/seleniumhq/selenium/App.java
+++ src/main/java/org/seleniumhq/selenium/App.java
@@ -1,5 +1,8 @@
 package org.seleniumhq.selenium;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import org.openqa.selenium.By;
@@ -23,14 +26,24 @@ public class App {
 
         Calendar from = Calendar.getInstance();
         Calendar to= Calendar.getInstance();
+
+        Calendar from2 = Calendar.getInstance();
+        Calendar to2= Calendar.getInstance();
         from .add(Calendar.YEAR,-12);
-        Stock tesla = YahooFinance.get("AAPL", true);
-         String text;
-        System.out.println(tesla.getHistory(from,to));
 
+        from2.add(Calendar.YEAR,-5);
+        Stock apple = YahooFinance.get("f", true);
+        System.out.println(apple.getHistory(from,to));
 
-
-
+        BufferedReader reader = new BufferedReader(new FileReader("test.csv"));
+       // List<String> lines = new ArrayList<String>();
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+         //   lines.add(line);
+            System.out.println(line);
+            Stock line1= YahooFinance.get(line, true);
+            System.out.println(line1.getHistory(from,to));
+        }
 
     }
 
