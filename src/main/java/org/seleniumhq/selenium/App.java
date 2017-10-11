8e6b2a5ef15d480bcf292cc90670a51cab8fbbf3
Chris gutierrez <chris201234@gmail.com>
2017-10-10 20:00:14 -0500

    new file:   javacsv/com/csvreader/CsvReader$ColumnBuffer.class 	new file:   javacsv/com/csvreader/CsvReader$ComplexEscape.class 	new file:   javacsv/com/csvreader/CsvReader$DataBuffer.class 	new file:   javacsv/com/csvreader/CsvReader$HeadersHolder.class 	new file:   javacsv/com/csvreader/CsvReader$Letters.class 	new file:   javacsv/com/csvreader/CsvReader$RawRecordBuffer.class 	new file:   javacsv/com/csvreader/CsvReader$StaticSettings.class 	new file:   javacsv/com/csvreader/CsvReader$UserSettings.class 	new file:   javacsv/com/csvreader/CsvReader.class 	new file:   javacsv/com/csvreader/CsvWriter$Letters.class 	new file:   javacsv/com/csvreader/CsvWriter$UserSettings.class 	new file:   javacsv/com/csvreader/CsvWriter.class 	modified:   src/main/java/org/seleniumhq/selenium/App.java
    added int a library for writing csv files and created a csv file for
    stock information

--- src/main/java/org/seleniumhq/selenium/App.java
+++ src/main/java/org/seleniumhq/selenium/App.java
@@ -1,10 +1,12 @@
 package org.seleniumhq.selenium;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.BufferedReader;
+import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import com.csvreader.CsvWriter;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -19,6 +21,10 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
+import yahoofinance.histquotes.HistoricalQuote;
+import yahoofinance.histquotes.Interval;
+
+import static yahoofinance.histquotes.Interval.DAILY;
 
 
 public class App {
@@ -30,24 +36,54 @@ public class App {
         Calendar from2 = Calendar.getInstance();
 
         BufferedReader reader = new BufferedReader(new FileReader("test.csv"));
-        List<String> lines = new ArrayList<String>();
+        List<String> ticker = new ArrayList<String>();
+        List<HistoricalQuote> hist_data = new ArrayList<HistoricalQuote>();
+
         String line = null;
         Stock line1;
+        int index=0;
         while ((line = reader.readLine()) !=null ) {
             try{
-            line1 = YahooFinance.get(line ,from,to);}
+            //line1 = YahooFinance.get(line ,from,to);
+
+                line1 = YahooFinance.get(line,from,to);
+
+            }
             catch(IOException e){
                continue;
 
             }
-//            System.out.println(line1.getHistory(from,to));
-            line1 = YahooFinance.get(line,from,to);
-                lines.add(line1.getName()+" "+line1.getHistory(from,to));
-            System.out.println("this is the size of the input:"+lines.size());
-            System.out.println(line.toString());
+            catch(StringIndexOutOfBoundsException e){
+                break;
+            }
+                ticker.add(line1.getName());
+                hist_data.addAll(line1.getHistory(from,to));
+            System.out.println("this is the size of the input:"+ticker.size());
+            System.out.println(ticker.get(index));
+            System.out.println(index);
+            index++;
 
         }
 
+
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd_HH.mm.ss");
+        File file = new File(  df.format(new Date()) +"_Stocks.csv");
+        if ( !file.exists() )
+            file.createNewFile();
+
+        // Use FileWriter constructor that specifies open for appending
+        CsvWriter csvOutput = new CsvWriter(new FileWriter(file, true), ',');
+       for(int i =0;i<ticker.size();i++){
+           csvOutput.write(ticker.get(i));
+
+           for(int x=0 ;x<hist_data.size();x++)
+           csvOutput.write(hist_data.get(x).getClose()+"");
+
+        csvOutput.endRecord();
+       }
+
+        csvOutput.flush();
+        csvOutput.close();
 
     }
 
