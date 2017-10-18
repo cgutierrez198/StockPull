e2c1d91dada58f1940984f2c93eaa90a89ae6f36
Chris gutierrez <chris201234@gmail.com>
2017-10-17 19:05:15 -0500

    formats to csv 	old format replicated same out put for each ticker

--- src/main/java/org/seleniumhq/selenium/App.java
+++ src/main/java/org/seleniumhq/selenium/App.java
@@ -1,17 +1,17 @@
 package org.seleniumhq.selenium;
 import java.io.*;
+import java.math.BigDecimal;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import com.csvreader.CsvWriter;
+import com.csvreader.*;
 import java.io.IOException;
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
 import yahoofinance.histquotes.HistoricalQuote;
-import yahoofinance.histquotes.Interval;
 
 
 
@@ -20,21 +20,26 @@ public class App {
 
         Calendar from = Calendar.getInstance();
         Calendar to= Calendar.getInstance();
-        from.add(Calendar.YEAR, -12);
-        Calendar from2 = Calendar.getInstance();
+        from.add(Calendar.YEAR, -2);
 
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd_HH.mm.ss");
+        File file = new File(  df.format(new Date()) +"_Stocks.csv");
+        if ( !file.exists() )
+            file.createNewFile();
         BufferedReader reader = new BufferedReader(new FileReader("test.csv"));
+        List<HistoricalQuote> date = new ArrayList<HistoricalQuote>();
+//        List<HistoricalQuote> hist_data = new ArrayList<HistoricalQuote>();
+        List<HistoricalQuote> hist_data;
         List<String> ticker = new ArrayList<String>();
-        List<HistoricalQuote> hist_data = new ArrayList<HistoricalQuote>();
 
+        CsvWriter csvOutput = new CsvWriter(new FileWriter(file, true), ',');
         String line = null;
         Stock line1;
         int index=0;
-        while ((line = reader.readLine()) !=null ) {
-            try{
-            //line1 = YahooFinance.get(line ,from,to);
 
-                line1 = YahooFinance.get(line,from,to,Interval.DAILY);
+        while ((line = reader.readLine()) !=null ) {
+            try {
+                line1 = YahooFinance.get(line, from, to);
 
             }
             catch(IOException e){
@@ -44,35 +49,35 @@ public class App {
             catch(StringIndexOutOfBoundsException e){
                 break;
             }
-                ticker.add(line1.getName());
-                hist_data.addAll(line1.getHistory(from,to, Interval.DAILY));
+
+
+
+//            csvOutput.endRecord();
+
+            // Use FileWriter constructor that specifies open for appending
+            hist_data = new ArrayList<HistoricalQuote>(line1.getHistory(from,to));
+            if(index<1){
+                csvOutput.write("");
+             for(int i =0;i<hist_data.size();i++){
+                 csvOutput.write(hist_data.get(i).getDate().getTime()+"");
+             }
+             csvOutput.endRecord();
+            }
+
+            csvOutput.write(line1.getName());
+            for(int i=0;i< hist_data.size();i++)
+            csvOutput.write(hist_data.get(i).getClose()+ "");
+            csvOutput.endRecord();
+               /*
             System.out.println("this is the size of the input:"+ticker.size());
             System.out.println(ticker.get(index));
             System.out.println(index);
             index++;
-
+            */
+            index++;
         }
 
 
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd_HH.mm.ss");
-        File file = new File(  df.format(new Date()) +"_Stocks.csv");
-        if ( !file.exists() )
-            file.createNewFile();
-
-        // Use FileWriter constructor that specifies open for appending
-        CsvWriter csvOutput = new CsvWriter(new FileWriter(file, true), ',');
-       for(int i =0;i<ticker.size();i++){
-           csvOutput.write(ticker.get(i));
-
-           for(int x=0 ;x<hist_data.size();x++)
-           csvOutput.write(hist_data.get(x).getClose()+"");
-
-        csvOutput.endRecord();
-       }
-
-        csvOutput.flush();
-        csvOutput.close();
 
     }
-
 }
